4218a70bcb503f59076582e58f6c00a8c99d794e
Lucas Skoff <lucasskoff@gmail.com>
2019-05-07 18:35:16 -0500

    Refactored test cases to reduce redundancy.

--- src/test/BabysitterTest.java
+++ src/test/BabysitterTest.java
@@ -1,28 +1,31 @@
 package test;
 
 import code.BabysitterTimesheet;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class BabysitterTest {
+    private BabysitterTimesheet babysitterTimesheet;
+    @Before
+    public void setup() {
+       babysitterTimesheet = new BabysitterTimesheet();
+    }
 
     @Test
     public void givenStartTimeWhenTimeIsBefore5ThenReturnFalse() {
-        BabysitterTimesheet babysitterTimesheet = new BabysitterTimesheet();
         assertFalse(babysitterTimesheet.isValidStartTime(16));
     }
 
     @Test
     public void givenStartTimeWhenTimeEquals5ThenReturnTrue() {
-        BabysitterTimesheet babysitterTimesheet = new BabysitterTimesheet();
         assertTrue(babysitterTimesheet.isValidStartTime(17));
     }
 
     @Test
     public void givenStartTimeWhenTimeIsAfter5ThenReturnTrue() {
-        BabysitterTimesheet babysitterTimesheet = new BabysitterTimesheet();
         assertTrue(babysitterTimesheet.isValidStartTime(18));
     }
 }
