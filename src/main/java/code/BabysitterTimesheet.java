19e2cc8cbbde06a32493a2bd1619e9de3e50032d
Lucas Skoff <lucasskoff@gmail.com>
2019-05-08 18:42:05 -0500

    Added BabysitterTimesheet back to subversion.

---
+++ src/main/java/code/BabysitterTimesheet.java
@@ -0,0 +1,14 @@
+package code;
+
+public class BabysitterTimesheet {
+    private static final int EARLIEST_START_TIME = 17;
+    private static final int LATEST_END_TIME = 4;
+    private static final int MIDNIGHT = 24;
+    public boolean isValidStartTime(int time) {
+        return time >= EARLIEST_START_TIME;
+    }
+
+    public boolean isValidEndTime(int time) {
+        return (time >= EARLIEST_START_TIME && time < MIDNIGHT) || time <= LATEST_END_TIME;
+    }
+}
