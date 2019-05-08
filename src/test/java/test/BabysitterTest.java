e30614c4c0063ade63caae92dfcddf6b1472a964
Lucas Skoff <lucasskoff@gmail.com>
2019-05-08 18:33:26 -0500

    Added maven support. Changed project structure to match Maven specification.

--- src/test/java/test/BabysitterTest.java
+++ src/test/java/test/BabysitterTest.java
@@ -3,7 +3,6 @@ package test;
 import code.BabysitterTimesheet;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
