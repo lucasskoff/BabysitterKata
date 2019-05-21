851c7160afd50e529ad8032fe56549e6d5aba00d
Lucas Skoff <lucasskoff@gmail.com>
2019-05-21 10:45:25 -0500

    Created test class for Family Code B

---
+++ src/test/java/test/FamilyCodeBTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import code.FamilyCodeA;
+import code.FamilyCodeB;
+import code.FamilyInterface;
+import constants.FamilyPayConstants;
+import constants.TimeConstants;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FamilyCodeBTest {
+
+    private FamilyInterface familyCodeB;
+
+    @Before
+    public void setup() {
+        familyCodeB = new FamilyCodeB();
+    }
+
+    @Test
+    public void givenEarliestHourReturnFamilyCodeBBasePay() {
+        int actual = familyCodeB.computePay(TimeConstants.EARLIEST_START_TIME);
+        int expected = FamilyPayConstants.FAMILY_B_BASE_PAY;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenTimeAfter10ReturnFamilyCodeBTimeBetween10And11Pay() {
+        int actual = familyCodeB.computePay(TimeConstants.ELEVEN_PM);
+        int expected = FamilyPayConstants.FAMILY_B_BETWEEN_10_PM_AND_MIDNIGHT_PAY;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenTimeAfterMidnightReturnFamilyCodeBTimeAfterMidnightPay() {
+        int actual = familyCodeB.computePay(TimeConstants.MIDNIGHT + 1);
+        int expected = FamilyPayConstants.FAMILY_B_POST_MIDNIGHT_PAY;
+        assertEquals(expected, actual);
+    }
+}
